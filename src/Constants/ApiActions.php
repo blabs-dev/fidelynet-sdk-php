2b417985e7aae53451d84e74bba2e17f11ea45d6
Salvo Bonanno <salvob.labs@gmail.com>
2021-07-25 17:04:41 +0200

    Renamed backoffice login action constant, added new ones

--- src/Constants/ApiActions.php
+++ src/Constants/ApiActions.php
@@ -5,11 +5,17 @@ namespace Blabs\FidelyNet\Constants;
 final class ApiActions
 {
     const LOGIN = 'login';
-    const LOGINBO = 'loginbo';
-    const SYNCHRO = 'synchro';
     const GET_CAMPAIGN = 'getcampaign';
-    const VERIFY_EMAIL = 'verifyemail';
-    const VERIFY_PHONE = 'verifyemail';
-    const REGISTER_WITH_VC = 'registrationvcwithverification';
     const GET_MOVEMENTS = 'getmovements';
+
+    const SYNCHRO = 'synchro';
+
+    const VERIFY_EMAIL = 'verifyemail';
+    const VERIFY_PHONE = 'verifyphone';
+    const REGISTER_WITH_CODE = 'registrationvcwithverification';
+    const REGISTER_WITHOUT_CODE = 'registrationvc';
+
+    const BO_LOGIN = 'loginbo';
+    const BO_MODIFY_CUSTOMER = 'modifycustomerbo';
+    const BO_GET_INFO_CARD = 'getinfocardbo2';
 }
