7779265ff9c5f6edd6d2a1ad0d2b316f096b6a98
Salvo Bonanno <salvob.labs@gmail.com>
2021-07-27 16:53:10 +0200

    Fixed version number

--- src/Constants/Release.php
+++ src/Constants/Release.php
@@ -4,8 +4,8 @@ namespace Blabs\FidelyNet\Constants;
 
 final class Release
 {
-    const MAJOR = 0;
-    const MINOR = 2;
-    const PATCH = 0;
+    const MAJOR = 1;
+    const MINOR = 0;
+    const PATCH = 1;
     const USER_AGENT = 'B@Labs FidelyNET Client v'.self::MAJOR.'.'.self::MINOR.'.'.self::PATCH;
 }
