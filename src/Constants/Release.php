a180e8b9cae8355e04e0ebe783b765847d128404
Salvo Bonanno <salvob.labs@gmail.com>
2021-07-28 07:52:46 +0200

    Updated to 2.0.3

--- src/Constants/Release.php
+++ src/Constants/Release.php
@@ -6,6 +6,6 @@ final class Release
 {
     const MAJOR = 2;
     const MINOR = 0;
-    const PATCH = 2;
+    const PATCH = 3;
     const USER_AGENT = 'B@Labs FidelyNET Client v'.self::MAJOR.'.'.self::MINOR.'.'.self::PATCH;
 }
